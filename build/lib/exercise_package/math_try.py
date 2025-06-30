b9282e130590dc776d10969dbec09a82f6118152
mdiby@stsci.edu <mdiby@stsci.edu>
2025-06-30 08:56:35 -0400

    changes on package set up

---
+++ build/lib/exercise_package/math_try.py
@@ -0,0 +1,10 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 28 06:56:35 2025
+
+@author: mdiby
+"""
+
+def add(a, b):
+    return a + b
---
+++ build/lib/exercise_package/pythonscript1.py
@@ -0,0 +1,8 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 15 17:34:55 2025
+
+@author: mdiby
+"""
+
